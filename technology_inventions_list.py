28ad75167063c6cba908d582c98a4e444d768d68
christian valzado <christianvalzado2@gmail.com>
2024-09-22 01:42:42 -0700

    technology_inventions_list.py

---
+++ technology_inventions_list.py
@@ -0,0 +1,33 @@
+# technology_inventions_list.py
+
+# Create a list of 15 modern technology inventions
+inventions = [
+    "Personal Computer",
+    "Internet",
+    "Smart Home Device",
+    "Tablet",
+    "Smartwatch",
+    "3D Printer",
+    "Virtual Reality Headset",
+    "Drone",
+    "Artificial Intelligence",
+    "Blockchain",
+    "Cloud Computing",  # 11th index
+    "Wearable Technology",
+    "Augmented Reality",
+    "Electric Vehicle",
+    "Robotics"
+]
+print(inventions)
+
+print("the 11th index is",inventions[10])
+print(inventions)
+
+inventions[7]= "Smartphones"
+print(inventions)
+
+del inventions[13]
+
+print(invention[5:10])
+
+print("the last index is",inventions[-1])
