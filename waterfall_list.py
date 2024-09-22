07e750720160fc2090716a71911de5a1a883b07a
christian valzado <christianvalzado2@gmail.com>
2024-09-22 02:50:28 -0700

    waterfall_list.py

---
+++ waterfall_list.py
@@ -0,0 +1,26 @@
+# waterfall_list.py
+
+# Create a list of 10 famous waterfalls
+waterfalls = [
+    "Angel Falls",
+    "Victoria Falls",
+    "Iguazu Falls",
+    "Yosemite Falls",
+    "Ban Gioc Falls",  # This will be updated
+    "Pagsanjan falls",
+    "Sutherland Falls",
+    "Plitvice Falls",
+    "Skógafoss",
+    "Kaaterskill Falls"
+]
+print(waterfalls)
+
+print("the 8th index is"waterfalls[7])
+print(waterfalls)
+
+waterfalls[4]= "Niagara Falls"
+print(waterfalls)
+
+del waterfalls[8]
+
+print("the last index is",waterfalls[-1])
