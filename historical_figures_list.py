1c84c14fb74f83926914b4c955941de2b232d7cf
christian valzado <christianvalzado2@gmail.com>
2024-09-22 00:43:21 -0700

    historical_figures_list.py

---
+++ historical_figures_list.py
@@ -0,0 +1,26 @@
+# historical_figures_list.py
+
+# Create a list of 10 historical figures
+historical_figures = [
+    "George Washington",
+    "Cleopatra",
+    "Mahatma Gandhi",
+    "Albert Einstein",
+    "Marie Curie",
+    "Winston Churchill",
+    "Martin Luther King Jr.",
+    "Rosa Parks",
+    "Leonardo da Vinci",
+    "Marie Antoinette"
+]
+print(historical_figures)
+
+print("the 8th index is",historical_figures[7])
+print(historical_figures)
+
+historical_figures[3] = "Nelson Mandela"
+print(historical_figures)
+
+del historical_figures[6]
+
+print("the last index is",historical_figures[-1])
