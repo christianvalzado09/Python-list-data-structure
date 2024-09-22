af6b8acd7404ff46c0a103fc8b74fb5ab3e18006
christian valzado <christianvalzado2@gmail.com>
2024-09-22 02:01:15 -0700

    currencies_list.py

---
+++ currency_list.py
@@ -0,0 +1,27 @@
+# currency_list.py
+
+# Create a list of 10 world currencies
+currencies = [
+    "US Dollar",
+    "Japanese Yen",
+    "British Pound",
+    "Australian Dollar",
+    "Canadian Dollar",
+    "Swiss Franc",
+    "Brazilian Real",
+    "Chinese Yuan",
+    "Indian Rupee",
+    "South Korean Won"
+]
+print(currencies)
+
+print("the 4th index is",currencies[3])
+print(currencies)
+
+currencies[6]= "Euro"
+print(currencies)
+
+del currencies[8]
+
+print("the last index is",currencies[-1])
+
