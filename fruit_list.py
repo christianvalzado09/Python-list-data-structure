a194959ecba2f1bb120c21b2339d7891d2a7d989
christian valzado <christianvalzado2@gmail.com>
2024-09-21 23:09:22 -0700

    fruits_list.py

---
+++ fruit_list.py
@@ -0,0 +1,16 @@
+fruits_list = [
+    "Apple", "Banana", "Orange", "Grapes", "Pineapple",
+    "Strawberry", "Watermelon", "Cherry", "Papaya",
+    "Peach", "Kiwi", "Blueberry"
+]
+print(fruits_list)
+
+print("the 9th index is",fruits_list[8])
+print(fruits_list)
+
+fruits_list[1] = "mango"
+print(fruits_list)
+
+print(fruits_list[4:7])
+
+print("the last index is",fruits_list[-1])
