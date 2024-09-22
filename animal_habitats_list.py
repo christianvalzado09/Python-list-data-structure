f362e638871697d6937029ed41e737eb11d6ee56
christian valzado <christianvalzado2@gmail.com>
2024-09-22 01:46:09 -0700

    animal_habitats_list.py

---
+++ animal_habitats_list.py
@@ -0,0 +1,24 @@
+# animal_habitats.py
+
+# Create a list of 8 animal habitats
+habitats = [
+    "Forest",
+    "Desert",
+    "Rainforest",
+    "Wetland",
+    "Ocean",
+    "Grassland",
+    "Mountain",
+    "Tundra"
+]
+print(habitats)
+
+print("the 5th index is"habitats[4])
+print(habitats)
+
+habitats[2]= "Savannah"
+print(habitats)
+
+del habitats[5]
+
+print("the last index is",habitats[-1])
