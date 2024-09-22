6c577866cf0726b21d1b25e87af373fdbb8154c2
christian valzado <christianvalzado2@gmail.com>
2024-09-22 02:11:00 -0700

    drinks_list.py

---
+++ drink_list.py
@@ -0,0 +1,23 @@
+# drink_list.py
+
+# Create a list of 8 popular drinks
+drinks = [
+    "Water",
+    "Tea",
+    "Juice",
+    "Soda",
+    "Milk",
+    "Lemonade",
+    "Wine",
+    "Lemon tea"
+]
+print(drinks)
+
+print("the 4th index is",drinks[3])
+print(drinks)
+
+drinks[2]= "Coffee"
+print(drinks)
+
+del drinks[6]
+print("the last index is",drinks[-1])
