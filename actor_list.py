caa5863dbfe21f00938d2cb9dc4e54c63ff44bf4
christian valzado <christianvalzado2@gmail.com>
2024-09-22 01:30:28 -0700

    actor_list.py

---
+++ actor_list.py
@@ -0,0 +1,25 @@
+# actor_list.py
+
+# Create a list of 10 famous actors
+actors = [
+    "Robert Downey Jr.",
+    "Scarlett Johansson",
+    "Tom Hanks",
+    "Brad Pitt",
+    "Angelina Jolie",
+    "Johnny Depp",
+    "Morgan Freeman",
+    "Meryl Streep",
+    "Denzel Washington",
+    "Hugh Jackman"
+]
+print(actors)
+
+print("the 7th index is",actors[6])
+print(actors)
+
+actors[2]=  "Leonardo DiCaprio"
+print(actors)
+
+del actors[7]
+actors("the last index is",actors[-1])
