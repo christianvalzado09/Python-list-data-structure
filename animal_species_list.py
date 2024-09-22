aa6981ff4e32416782ac9f161d72e7296a323dba
christian valzado <christianvalzado2@gmail.com>
2024-09-22 00:37:48 -0700

    animal_species_list.py

---
+++ animal_species_list.py
@@ -0,0 +1,24 @@
+# animal_species.py
+
+# Create a list of 7 animal species
+animals = [
+    "Lion",
+    "Elephant",
+    "Tiger",
+    "Zebra",
+    "Kangaroo",
+    "Panda",
+    "Giraffe"
+]
+
+print(animals)
+
+print("the 2nd index is",animals[1])
+print(animals)
+
+animals[4]= "Gorilla"
+print(animals)
+
+del animals[5]
+
+print("the last index is",animals[-1])
