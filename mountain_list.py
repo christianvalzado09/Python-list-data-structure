dc3156c84419f5b7e6f74e6a255477624207965d
christian valzado <christianvalzado2@gmail.com>
2024-09-22 00:24:14 -0700

    mountain_list.py

---
+++ mountain_list.py
@@ -0,0 +1,22 @@
+# Create a list of 8 mountain names
+mountain_list= [
+    "Kilimanjaro",
+    "K2",
+    "Denali",
+    "Fuji",
+    "Aconcagua",
+    "Elbrus",
+    "Annapurna",
+    "Mont Blanc"
+]
+print(mountain_list)
+
+print("the 5th index is",mountain_list[4])
+print(mountain_list)
+
+mountain_list[2] = "Everest"
+print(mountain_list)
+
+del mountain_list[5]
+
+print("the last index is",mountain_list[-1])
