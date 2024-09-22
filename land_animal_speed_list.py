fd521943b68e90ae603a9e05111f880c132ca5e7
christian valzado <christianvalzado2@gmail.com>
2024-09-22 02:39:20 -0700

    land_animal_speed_list.py

---
+++ land_animal_speed_list.py
@@ -0,0 +1,31 @@
+# land_animal_speed_list.py
+
+# Create a list of 12 land animals and their speeds
+animals = [
+    "Elephant - 40 km/h",
+    "Lion - 50 km/h",
+    "Giraffe - 60 km/h",
+    "Zebra - 65 km/h",
+    "Kangaroo - 70 km/h",
+    "Horse - 88 km/h",
+    "Gray Wolf - 60 km/h",
+    "Rhino - 40 km/h",
+    "Buffalo - 40 km/h",
+    "Bison - 56 km/h",
+    "Tiger - 65 km/h",
+    "Wild Boar - 30 km/h"
+]
+
+print(animals)
+
+print("the 7th index is",animals[6])
+print(animals)
+
+animals[8]=  "Cheetah - 120 km/h"
+print(animals)
+
+del animals[9]
+print(animals[3:7])
+
+print("the last index is",animals[-1])
+
