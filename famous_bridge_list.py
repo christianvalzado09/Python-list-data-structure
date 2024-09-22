8964b4822dd6ca486bdc68cf5867180ed06f850e
christian valzado <christianvalzado2@gmail.com>
2024-09-22 02:54:08 -0700

    famous_bridge_list.py

---
+++ famous_bridge_list.py
@@ -0,0 +1,24 @@
+# famous_bridge_list.py
+
+# Create a list of 8 famous bridges
+bridges = [
+    "Brooklyn Bridge",
+    "London Bridge",
+    "Tower Bridge",
+    "Sydney Harbour Bridge",
+    "Charles Bridge",
+    "Forth Bridge",
+    "Bosphorus Bridge",
+    "Clifton Suspension Bridge"
+]
+print(bridges)
+
+print("the 3rd index is",bridges[2])
+print(bridges)
+
+bridges[5]= "Golden Gate Bridge"
+print(bridges)
+
+del bridges[6]
+
+print("the last index is",bridges[-1])
