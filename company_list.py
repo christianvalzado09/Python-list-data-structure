a425e3d0c0a99b6f5aa72a0f8af8f3a809364101
christian valzado <christianvalzado2@gmail.com>
2024-09-22 00:28:52 -0700

    company_list.py

---
+++ company_list.py
@@ -0,0 +1,26 @@
+# company_list.py
+
+# Create a list of 10 company names
+company_list = [
+    "Google",
+    "Microsoft",
+    "Amazon",
+    "Facebook",
+    "Tesla",
+    "Netflix",
+    "IBM",
+    "Samsung",
+    "Intel",
+    "Oracle"
+    ]
+print(company_list)
+
+print("the 7th index is",[6])
+print(company_list)
+
+company_list[4] = "Apple"
+print(company_list)
+
+del company_list[7]
+
+print("the last index is",company_list[-1])
