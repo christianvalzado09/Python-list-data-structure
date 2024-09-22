9e211acc7b3f504fcbf887dbef36ec9a208a9c5b
christian valzado <christianvalzado2@gmail.com>
2024-09-21 22:31:01 -0700

    studentsname.py

---
+++ studentsname.py
@@ -0,0 +1,16 @@
+student_names = [ "paul","chan","arlyn","kyle","zery,","christian","chloe","justine","daisy","carlo","emman","brando","patrick","aaron","cris","danaya","mary","andrea","nicole","jerome"]
+
+print(student_names)
+
+print("the 15 index is ",student_names[14])
+
+student_names[12] = "john"
+print(student_names)
+
+del student_names[9]
+print(student_names)
+
+print(student_names[2:5])
+
+
+print("the last index is",student_names[-1])
