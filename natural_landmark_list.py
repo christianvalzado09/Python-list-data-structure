374270e759059bf029453f02573062648b5af4ce
christian valzado <christianvalzado2@gmail.com>
2024-09-22 02:33:47 -0700

    natural_landmarks_list.py

---
+++ natural_landmark_list.py
@@ -0,0 +1,26 @@
+# natural_landmark_list.py
+
+# Create a list of 10 natural landmarks
+landmarks = [
+    "Mountain Fuji",
+    "Great Barrier Reef",
+    "Mount Everest",
+    "Victoria Falls",
+    "Salar de Uyuni",
+    "Banff National Park",
+    "Yellowstone National Park",
+    "Yosemite National Park",
+    "Machu Picchu",
+    "Zhangjiajie National Forest"
+]
+print(landmarks)
+
+print("the 8th inde is",landmarks[7])
+print(landmarks)
+
+landmarks[4]= "Grand Canyon"
+print(landmarks)
+
+del landmarks[8]
+
+print("the last index is",landmarks[-1])
