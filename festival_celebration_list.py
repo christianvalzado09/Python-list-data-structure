f3f923e22fcf6dd6919804ce43d431cddb7ab14d
christian valzado <christianvalzado2@gmail.com>
2024-09-22 02:19:45 -0700

    festival_celebration_list.py

---
+++ festival_celebration_list.py
@@ -0,0 +1,25 @@
+# festival_celebration_list.py
+
+# Create a list of 10 festival celebrations
+festivals = [
+    "Diwali",
+    "Christmas",
+    "Eid al-Fitr",
+    "Holi",
+    "New Year",
+    "Hanukkah",
+    "Mardi gras",
+    "Lunar New Year",
+    "Oktoberfest",
+    "Bastille Day"
+]
+print(festivals)
+
+print("the 5th index is",festivals[4])
+print(festivals)
+
+festivals[2]= "Thanksgiving"
+print(festivals)
+
+del festivals[6]
+print("the last index is",festivals[-1])
