589c72f6fddbe919fd687cb4aa831f3dce5b4ccb
christian valzado <christianvalzado2@gmail.com>
2024-09-22 01:38:33 -0700

    tool_list.py

---
+++ tool_list.py
@@ -0,0 +1,26 @@
+# tool_list.py
+
+# Create a list of 10 tools used in carpentry
+tools = [
+    "Saw",
+    "Nail Gun",
+    "Chisel",
+    "Screwdriver",
+    "Tape Measure",
+    "Level",
+    "Pliers",
+    "Wrench",
+    "Drill",
+    "Square"
+]
+print(tools)
+
+print("the 4th index is",tools[3])
+print(tools)
+
+tools[6]= "Hammer"
+print(tools)
+
+del tools[4]
+
+print("the last index is",tools[-1])
