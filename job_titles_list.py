4a6be9f7f50db6a6eeab67bfaf3f82cfb04dc0e8
christian valzado <christianvalzado2@gmail.com>
2024-09-22 01:50:29 -0700

    job_titles_list.py

---
+++ job_titles_list.py
@@ -0,0 +1,26 @@
+# job_titles_list.py
+
+# Create a list of 10 job titles
+job_titles = [
+    "Teacher",
+    "Nurse",
+    "Engineer",
+    "Doctor",
+    "Designer",
+    "Civil engineer",
+    "Marketing Manager",
+    "Data Scientist",
+    "Project Manager",
+    "Sales Representative"
+]
+print(job_titles)
+
+print("the 6th index is",job_titles[5])
+print(job_titles)
+
+job_titles[4]= "Software Engineer"
+print(job_titles)
+
+del job_titles[8]
+
+print("the lasy index is",job_titles[-1])
