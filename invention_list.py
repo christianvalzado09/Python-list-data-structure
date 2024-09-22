75306110618060b09b6995e1a9bb02a864dc57f5
christian valzado <christianvalzado2@gmail.com>
2024-09-22 00:48:34 -0700

    invention_list.py

---
+++ invention_list.py
@@ -0,0 +1,24 @@
+# invention_list.py
+
+# Create a list of 8 famous inventions
+inventions = [
+    "Wheel",
+    "Printing Press",
+    "Telephone",
+    "Airplane",
+    "Computer",
+    "Internet",
+    "Television",
+    "Radio"
+]
+print(inventions)
+
+print("the 6th index is"inventions[5])
+print(inventions)
+
+inventions[1] = "Lightbulb"
+print(inventions)
+
+del inventions[4]
+
+print("the last index is",inventions[-1])
