8ce08baa6aab41c1e4c479d942dc0fa5fb498063
christian valzado <christianvalzado2@gmail.com>
2024-09-21 23:31:43 -0700

    game_list.py

---
+++ game_list.py
@@ -0,0 +1,30 @@
+
+game_list= [
+    "The Legend of Zelda: Breath of the Wild",
+    "The Witcher 3: Wild Hunt",
+    "Red Dead Redemption 2",
+    "Grand Theft Auto V",
+    "Dark Souls",
+    "Fortnite",
+    "Call of Duty: Modern Warfare",
+    "Overwatch",
+    "God of War",
+    "Hollow Knight",
+    "Cyberpunk 2077",
+    "Among Us",
+    "Resident Evil 2",
+    "Super Mario Odyssey",
+    "Apex Legends"
+]
+
+print(game_list)
+
+print("the 7th index is",game_list[6])
+print(game_list)
+
+game_list[8] = "minecraft"
+print(game_list)
+
+print(game_list[5:10])
+
+print("the last index is",game_list[-1])
