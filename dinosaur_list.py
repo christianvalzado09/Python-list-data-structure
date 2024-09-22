f0f29903ce191c190df3cfab122d598f6d001d25
christian valzado <christianvalzado2@gmail.com>
2024-09-22 02:29:53 -0700

    dinosaur_list.py

---
+++ dinosaur_list.py
@@ -0,0 +1,23 @@
+# dinosaur_list.py
+
+# Create a list of 8 dinosaur names
+dinosaurs = [
+    "Diplodocus",
+    "Velociraptor",
+    "Triceratops",
+    "Stegosaurus",
+    "Brachiosaurus",
+    "Allosaurus",
+    "Ankylosaurus",
+    "Spinosaurus"
+]
+print(dinosaurs)
+
+print("the 4th index is",dinosaurs[3])
+print(dinosaurs)
+
+dinosaurs[5]= "Tyrannosaurus Rex"
+print(dinosaurs)
+
+del dinosaurs[6]
+print("the last index is",dinosaurs[-1])
