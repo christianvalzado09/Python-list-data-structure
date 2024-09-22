1da50aae3daf8362a9bdc62baa928c9855d2bd88
christian valzado <christianvalzado2@gmail.com>
2024-09-22 01:10:12 -0700

    furniture_list.py

---
+++ furniture_list.py
@@ -0,0 +1,28 @@
+# furniture_list.py
+
+# Create a list of 12 furniture items
+furniture = [
+    "Chair",
+    "Table",
+    "Desk",
+    "Bed",
+    "Bookshelf",
+    "Cabinet",
+    "Dresser",
+    "Couch",
+    "Ottoman",
+    "Nightstand",
+    "Coffee Table",
+    "Bench"
+]
+print(furniture)
+
+print("the 8th index is",furniture[7])
+print(furniture)
+
+furniture[4]= "Sofa"
+print(furniture)
+
+del furniture[9]
+
+print("the last index is",furniture[-1])
