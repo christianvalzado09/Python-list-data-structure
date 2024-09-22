fc39a5c860589b647776ed68bae37849bfa798da
christian valzado <christianvalzado2@gmail.com>
2024-09-22 01:18:23 -0700

    sports_list.py

---
+++ sports_list.py
@@ -0,0 +1,26 @@
+# sports_list.py
+
+# Create a list of 10 popular sports
+sports = [
+    "Soccer",
+    "Billiards",
+    "Tennis",
+    "Cricket",
+    "Baseball",
+    "Rugby",
+    "Golf",
+    "Swimming",
+    "Table Tennis",
+    "Volleyball"
+]
+print(sports)
+
+print("the 6th index is",sports[5])
+print(sports)
+
+sports[3]= "Basketball"
+print(sports)
+
+del sports[8]
+
+print("the last index is",sports[-1])
